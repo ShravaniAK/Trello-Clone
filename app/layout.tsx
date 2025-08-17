d9f10745a735749cd01c4189ac1d78b5c9ef236d
SHRAVANI KAWARE <97099574+ShravaniAK@users.noreply.github.com>
2025-08-17 23:27:09 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import SupabaseProvider from "@/lib/supabase/SupabaseProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +32,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <SupabaseProvider>{children}</SupabaseProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
